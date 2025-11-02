73b65456e9f5e9435f9c403805b8454b924b1409
Navid M <navidmomtahen@gmail.com>
2025-11-02 12:55:36 +0000

    fmt

--- src/raylib_bindings.ads
+++ src/raylib_bindings.ads
@@ -3,7 +3,7 @@ with Raylib_Types;
 
 package Raylib_Bindings is
    pragma Preelaborate;
-   
+
    use type Raylib_Types.Color;
    use type Raylib_Types.Vector2;
    use type Raylib_Types.Vector3;
@@ -13,77 +13,102 @@ package Raylib_Bindings is
    use type Raylib_Types.Font;
    use type Raylib_Types.Camera2D;
    use type Raylib_Types.Camera3D;
-   
-   procedure Init_Window(width, height : Interfaces.C.int; title : Interfaces.C.char_array);
+
+   procedure Init_Window
+     (width, height : Interfaces.C.int; title : Interfaces.C.char_array);
    pragma Import (C, Init_Window, "InitWindow");
-   
+
    procedure Close_Window;
    pragma Import (C, Close_Window, "CloseWindow");
-   
+
    function Window_Should_Close return Interfaces.C.int;
    pragma Import (C, Window_Should_Close, "WindowShouldClose");
-   
-   procedure Set_Target_FPS(fps : Interfaces.C.int);
+
+   procedure Set_Target_FPS (fps : Interfaces.C.int);
    pragma Import (C, Set_Target_FPS, "SetTargetFPS");
-   
+
    function Get_FPS return Interfaces.C.int;
    pragma Import (C, Get_FPS, "GetFPS");
-   
+
    procedure Begin_Drawing;
    pragma Import (C, Begin_Drawing, "BeginDrawing");
-   
+
    procedure End_Drawing;
    pragma Import (C, End_Drawing, "EndDrawing");
-   
-   procedure Clear_Background(color : Raylib_Types.Color);
+
+   procedure Clear_Background (color : Raylib_Types.Color);
    pragma Import (C, Clear_Background, "ClearBackground");
-   
-   procedure Draw_Circle(center_x, center_y : Interfaces.C.int; radius : Interfaces.C.C_float; color : Raylib_Types.Color);
+
+   procedure Draw_Circle
+     (center_x, center_y : Interfaces.C.int;
+      radius             : Interfaces.C.C_float;
+      color              : Raylib_Types.Color);
    pragma Import (C, Draw_Circle, "DrawCircle");
-   
-   procedure Draw_Circle_V(center : Raylib_Types.Vector2; radius : Interfaces.C.C_float; color : Raylib_Types.Color);
+
+   procedure Draw_Circle_V
+     (center : Raylib_Types.Vector2;
+      radius : Interfaces.C.C_float;
+      color  : Raylib_Types.Color);
    pragma Import (C, Draw_Circle_V, "DrawCircleV");
-   
-   procedure Draw_Rectangle(pos_x, pos_y, width, height : Interfaces.C.int; color : Raylib_Types.Color);
+
+   procedure Draw_Rectangle
+     (pos_x, pos_y, width, height : Interfaces.C.int;
+      color                       : Raylib_Types.Color);
    pragma Import (C, Draw_Rectangle, "DrawRectangle");
-   
-   procedure Draw_Rectangle_V(position : Raylib_Types.Vector2; size : Raylib_Types.Vector2; color : Raylib_Types.Color);
+
+   procedure Draw_Rectangle_V
+     (position : Raylib_Types.Vector2;
+      size     : Raylib_Types.Vector2;
+      color    : Raylib_Types.Color);
    pragma Import (C, Draw_Rectangle_V, "DrawRectangleV");
-   
-   procedure Draw_Rectangle_Rec(rec : Raylib_Types.Rectangle; color : Raylib_Types.Color);
+
+   procedure Draw_Rectangle_Rec
+     (rec : Raylib_Types.Rectangle; color : Raylib_Types.Color);
    pragma Import (C, Draw_Rectangle_Rec, "DrawRectangleRec");
-   
-   procedure Draw_Line(start_pos_x, start_pos_y, end_pos_x, end_pos_y : Interfaces.C.int; color : Raylib_Types.Color);
+
+   procedure Draw_Line
+     (start_pos_x, start_pos_y, end_pos_x, end_pos_y : Interfaces.C.int;
+      color                                          : Raylib_Types.Color);
    pragma Import (C, Draw_Line, "DrawLine");
-   
-   procedure Draw_Triangle(v1, v2, v3 : Raylib_Types.Vector2; color : Raylib_Types.Color);
+
+   procedure Draw_Triangle
+     (v1, v2, v3 : Raylib_Types.Vector2; color : Raylib_Types.Color);
    pragma Import (C, Draw_Triangle, "DrawTriangle");
-   
-   function Load_Texture(file_name : Interfaces.C.char_array) return Raylib_Types.Texture;
+
+   function Load_Texture
+     (file_name : Interfaces.C.char_array) return Raylib_Types.Texture;
    pragma Import (C, Load_Texture, "LoadTexture");
-   
-   procedure Unload_Texture(texture : Raylib_Types.Texture);
+
+   procedure Unload_Texture (texture : Raylib_Types.Texture);
    pragma Import (C, Unload_Texture, "UnloadTexture");
-   
-   procedure Draw_Texture(texture : Raylib_Types.Texture; pos_x, pos_y : Interfaces.C.int; tint : Raylib_Types.Color);
+
+   procedure Draw_Texture
+     (texture      : Raylib_Types.Texture;
+      pos_x, pos_y : Interfaces.C.int;
+      tint         : Raylib_Types.Color);
    pragma Import (C, Draw_Texture, "DrawTexture");
-   
-   procedure Draw_Text(text : Interfaces.C.char_array; pos_x, pos_y, font_size : Interfaces.C.int; color : Raylib_Types.Color);
+
+   procedure Draw_Text
+     (text                    : Interfaces.C.char_array;
+      pos_x, pos_y, font_size : Interfaces.C.int;
+      color                   : Raylib_Types.Color);
    pragma Import (C, Draw_Text, "DrawText");
-   
-   function Measure_Text(text : Interfaces.C.char_array; font_size : Interfaces.C.int) return Interfaces.C.int;
+
+   function Measure_Text
+     (text : Interfaces.C.char_array; font_size : Interfaces.C.int)
+      return Interfaces.C.int;
    pragma Import (C, Measure_Text, "MeasureText");
-   
-   function Is_Key_Pressed(key : Interfaces.C.int) return Interfaces.C.int;
+
+   function Is_Key_Pressed (key : Interfaces.C.int) return Interfaces.C.int;
    pragma Import (C, Is_Key_Pressed, "IsKeyPressed");
-   
-   function Is_Key_Down(key : Interfaces.C.int) return Interfaces.C.int;
+
+   function Is_Key_Down (key : Interfaces.C.int) return Interfaces.C.int;
    pragma Import (C, Is_Key_Down, "IsKeyDown");
-   
-   function Is_Key_Released(key : Interfaces.C.int) return Interfaces.C.int;
+
+   function Is_Key_Released (key : Interfaces.C.int) return Interfaces.C.int;
    pragma Import (C, Is_Key_Released, "IsKeyReleased");
 
-   function Is_Key_Up(key : Interfaces.C.int) return Interfaces.C.int;
+   function Is_Key_Up (key : Interfaces.C.int) return Interfaces.C.int;
    pragma Import (C, Is_Key_Up, "IsKeyUp");
 
    function Get_Key_Pressed return Interfaces.C.int;
@@ -92,99 +117,133 @@ package Raylib_Bindings is
    function Get_Char_Pressed return Interfaces.C.int;
    pragma Import (C, Get_Char_Pressed, "GetCharPressed");
 
-   function Is_Mouse_Button_Pressed(button : Interfaces.C.int) return Interfaces.C.int;
+   function Is_Mouse_Button_Pressed
+     (button : Interfaces.C.int) return Interfaces.C.int;
    pragma Import (C, Is_Mouse_Button_Pressed, "IsMouseButtonPressed");
-   
+
    function Get_Mouse_Position return Raylib_Types.Vector2;
    pragma Import (C, Get_Mouse_Position, "GetMousePosition");
-   
+
    function Get_Mouse_X return Interfaces.C.int;
    pragma Import (C, Get_Mouse_X, "GetMouseX");
-   
+
    function Get_Mouse_Y return Interfaces.C.int;
    pragma Import (C, Get_Mouse_Y, "GetMouseY");
-   
+
    function Get_Mouse_Wheel_Move return Interfaces.C.int;
    pragma Import (C, Get_Mouse_Wheel_Move, "GetMouseWheelMove");
-   
+
    function Get_Screen_Width return Interfaces.C.int;
    pragma Import (C, Get_Screen_Width, "GetScreenWidth");
-   
+
    function Get_Screen_Height return Interfaces.C.int;
    pragma Import (C, Get_Screen_Height, "GetScreenHeight");
-   
-   procedure Begin_Mode2D(camera : Raylib_Types.Camera2D);
+
+   procedure Begin_Mode2D (camera : Raylib_Types.Camera2D);
    pragma Import (C, Begin_Mode2D, "BeginMode2D");
-   
+
    procedure End_Mode2D;
    pragma Import (C, End_Mode2D, "EndMode2D");
-   
-   procedure Begin_Mode3D(camera : Raylib_Types.Camera3D);
+
+   procedure Begin_Mode3D (camera : Raylib_Types.Camera3D);
    pragma Import (C, Begin_Mode3D, "BeginMode3D");
-   
+
    procedure End_Mode3D;
    pragma Import (C, End_Mode3D, "EndMode3D");
-   
+
    function Get_Time return Interfaces.C.C_float;
    pragma Import (C, Get_Time, "GetTime");
-   
+
    function Get_Frame_Time return Interfaces.C.C_float;
    pragma Import (C, Get_Frame_Time, "GetFrameTime");
-   
+
    procedure Init_Audio_Device;
    pragma Import (C, Init_Audio_Device, "InitAudioDevice");
-   
+
    procedure Close_Audio_Device;
    pragma Import (C, Close_Audio_Device, "CloseAudioDevice");
-   
-   function Load_Sound(file_name : Interfaces.C.char_array) return Raylib_Types.Sound;
+
+   function Load_Sound
+     (file_name : Interfaces.C.char_array) return Raylib_Types.Sound;
    pragma Import (C, Load_Sound, "LoadSound");
-   
-   procedure Play_Sound(sound : Raylib_Types.Sound);
+
+   procedure Play_Sound (sound : Raylib_Types.Sound);
    pragma Import (C, Play_Sound, "PlaySound");
-   
-   procedure Stop_Sound(sound : Raylib_Types.Sound);
+
+   procedure Stop_Sound (sound : Raylib_Types.Sound);
    pragma Import (C, Stop_Sound, "StopSound");
 
-   procedure Pause_Sound(sound : Raylib_Types.Sound);
+   procedure Pause_Sound (sound : Raylib_Types.Sound);
    pragma Import (C, Pause_Sound, "PauseSound");
 
-   procedure Resume_Sound(sound : Raylib_Types.Sound);
+   procedure Resume_Sound (sound : Raylib_Types.Sound);
    pragma Import (C, Resume_Sound, "ResumeSound");
 
-   procedure Set_Sound_Volume(sound : Raylib_Types.Sound; volume : Interfaces.C.C_float);
+   procedure Set_Sound_Volume
+     (sound : Raylib_Types.Sound; volume : Interfaces.C.C_float);
    pragma Import (C, Set_Sound_Volume, "SetSoundVolume");
 
-   procedure Set_Window_Title(title : Interfaces.C.char_array);
+   procedure Set_Window_Title (title : Interfaces.C.char_array);
    pragma Import (C, Set_Window_Title, "SetWindowTitle");
 
    procedure Toggle_Fullscreen;
    pragma Import (C, Toggle_Fullscreen, "ToggleFullscreen");
 
-   procedure Draw_Circle_Gradient(center_x, center_y : Interfaces.C.int; radius : Interfaces.C.C_float; color1, color2 : Raylib_Types.Color);
+   procedure Draw_Circle_Gradient
+     (center_x, center_y : Interfaces.C.int;
+      radius             : Interfaces.C.C_float;
+      color1, color2     : Raylib_Types.Color);
    pragma Import (C, Draw_Circle_Gradient, "DrawCircleGradient");
 
-   procedure Draw_Rectangle_Gradient(pos_x, pos_y, width, height : Interfaces.C.int; color1, color2 : Raylib_Types.Color);
+   procedure Draw_Rectangle_Gradient
+     (pos_x, pos_y, width, height : Interfaces.C.int;
+      color1, color2              : Raylib_Types.Color);
    pragma Import (C, Draw_Rectangle_Gradient, "DrawRectangleGradient");
 
-   procedure Draw_Rectangle_Lines(pos_x, pos_y, width, height : Interfaces.C.int; color : Raylib_Types.Color);
+   procedure Draw_Rectangle_Lines
+     (pos_x, pos_y, width, height : Interfaces.C.int;
+      color                       : Raylib_Types.Color);
    pragma Import (C, Draw_Rectangle_Lines, "DrawRectangleLines");
 
-   procedure Draw_Texture_Pro(texture : Raylib_Types.Texture; source, dest : Raylib_Types.Rectangle; origin : Raylib_Types.Vector2; rotation : Interfaces.C.C_float; tint : Raylib_Types.Color);
+   procedure Draw_Texture_Pro
+     (texture      : Raylib_Types.Texture;
+      source, dest : Raylib_Types.Rectangle;
+      origin       : Raylib_Types.Vector2;
+      rotation     : Interfaces.C.C_float;
+      tint         : Raylib_Types.Color);
    pragma Import (C, Draw_Texture_Pro, "DrawTexturePro");
-   
-   procedure Draw_Texture_Rec(texture : Raylib_Types.Texture; source : Raylib_Types.Rectangle; position : Raylib_Types.Vector2; tint : Raylib_Types.Color);
+
+   procedure Draw_Texture_Rec
+     (texture  : Raylib_Types.Texture;
+      source   : Raylib_Types.Rectangle;
+      position : Raylib_Types.Vector2;
+      tint     : Raylib_Types.Color);
    pragma Import (C, Draw_Texture_Rec, "DrawTextureRec");
-   
-   procedure Draw_Circle_Lines(center_x, center_y : Interfaces.C.int; radius : Interfaces.C.C_float; color : Raylib_Types.Color);
+
+   procedure Draw_Circle_Lines
+     (center_x, center_y : Interfaces.C.int;
+      radius             : Interfaces.C.C_float;
+      color              : Raylib_Types.Color);
    pragma Import (C, Draw_Circle_Lines, "DrawCircleLines");
-   
-   procedure Draw_Rectangle_Rounded(rec : Raylib_Types.Rectangle; roundness : Interfaces.C.C_float; segments : Interfaces.C.int; color : Raylib_Types.Color);
+
+   procedure Draw_Rectangle_Rounded
+     (rec       : Raylib_Types.Rectangle;
+      roundness : Interfaces.C.C_float;
+      segments  : Interfaces.C.int;
+      color     : Raylib_Types.Color);
    pragma Import (C, Draw_Rectangle_Rounded, "DrawRectangleRounded");
-   
-   procedure Draw_Text_Ex(font : Raylib_Types.Font; text : Interfaces.C.char_array; position : Raylib_Types.Vector2; font_size, spacing : Interfaces.C.C_float; tint : Raylib_Types.Color);
+
+   procedure Draw_Text_Ex
+     (font               : Raylib_Types.Font;
+      text               : Interfaces.C.char_array;
+      position           : Raylib_Types.Vector2;
+      font_size, spacing : Interfaces.C.C_float;
+      tint               : Raylib_Types.Color);
    pragma Import (C, Draw_Text_Ex, "DrawTextEx");
 
-   function Measure_Text_Ex(font : Raylib_Types.Font; text : Interfaces.C.char_array; font_size, spacing : Interfaces.C.C_float) return Raylib_Types.Vector2;
+   function Measure_Text_Ex
+     (font               : Raylib_Types.Font;
+      text               : Interfaces.C.char_array;
+      font_size, spacing : Interfaces.C.C_float) return Raylib_Types.Vector2;
    pragma Import (C, Measure_Text_Ex, "MeasureTextEx");
 end Raylib_Bindings;
--- src/raylib_types.ads
+++ src/raylib_types.ads
@@ -6,21 +6,21 @@ package Raylib_Types is
    type Color is record
       r, g, b, a : Interfaces.C.unsigned_char;
    end record;
-   
+
    type Vector2 is record
       x, y : Interfaces.C.C_float;
    end record;
-   
+
    type Vector3 is record
       x, y, z : Interfaces.C.C_float;
    end record;
-   
+
    type Matrix is array (1 .. 4, 1 .. 4) of Interfaces.C.C_float;
-   
+
    type Rectangle is record
       x, y, width, height : Interfaces.C.C_float;
    end record;
-   
+
    type Texture is private;
    type Audio_Stream is private;
    type Sound is private;
@@ -30,49 +30,49 @@ package Raylib_Types is
 
 private
    type Texture is record
-      id : Interfaces.C.unsigned;
-      width : Interfaces.C.int;
-      height : Interfaces.C.int;
+      id      : Interfaces.C.unsigned;
+      width   : Interfaces.C.int;
+      height  : Interfaces.C.int;
       mipmaps : Interfaces.C.int;
-      format : Interfaces.C.int;
+      format  : Interfaces.C.int;
    end record;
-   
+
    type Texture_Access is access all Texture;
-   
+
    type Font is record
-      base_size : Interfaces.C.int;
-      glyph_count : Interfaces.C.int;
+      base_size     : Interfaces.C.int;
+      glyph_count   : Interfaces.C.int;
       glyph_padding : Interfaces.C.int;
-      texture : Texture_Access;
-      recs : access Rectangle;
-      glyphs : access Integer;
+      texture       : Texture_Access;
+      recs          : access Rectangle;
+      glyphs        : access Integer;
    end record;
-   
+
    type Camera3D is record
-      position : Vector3;
-      target : Vector3;
-      up : Vector3;
-      fovy : Interfaces.C.C_float;
+      position   : Vector3;
+      target     : Vector3;
+      up         : Vector3;
+      fovy       : Interfaces.C.C_float;
       projection : Interfaces.C.int;
    end record;
 
    type Camera2D is record
-      offset : Vector2;
-      target : Vector2;
+      offset   : Vector2;
+      target   : Vector2;
       rotation : Interfaces.C.C_float;
-      zoom : Interfaces.C.C_float;
+      zoom     : Interfaces.C.C_float;
    end record;
-   
+
    type Audio_Stream is record
-      buffer : access Interfaces.C.short;
-      processor : access Interfaces.C.unsigned;
+      buffer      : access Interfaces.C.short;
+      processor   : access Interfaces.C.unsigned;
       sample_rate : Interfaces.C.unsigned;
       sample_size : Interfaces.C.unsigned;
-      channels : Interfaces.C.unsigned;
+      channels    : Interfaces.C.unsigned;
    end record;
-   
+
    type Sound is record
-      stream : Audio_Stream;
+      stream      : Audio_Stream;
       frame_count : Interfaces.C.unsigned;
    end record;
 end Raylib_Types;
